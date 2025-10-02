90785bc92632bab6904e35684ed544d157961d64
A4BHI <manthukafc@gmail.com>
2025-10-02 15:02:26 +0530

    session

--- go/session/makesession.go
+++ go/session/makesession.go
@@ -1,3 +1,9 @@
 package session
 
-func SetSession(username string, mailid string)
+import "encoding/hex"
+
+func SetSession(username string, mailid string) bool {
+	randomb := make([]byte, 32)
+	sessionid := hex.EncodeToString(randomb)
+
+}
