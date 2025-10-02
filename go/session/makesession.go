88aafc2d34e55fe85d7dd9643e6ba21265ec2f15
A4BHI <manthukafc@gmail.com>
2025-10-02 20:43:33 +0530

    Updated code in register.go and makesession.go

--- go/session/makesession.go
+++ go/session/makesession.go
@@ -1,9 +1,38 @@
 package session
 
-import "encoding/hex"
+import (
+	"context"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"time"
+	"vaultx/db"
+)
 
-func SetSession(username string, mailid string) bool {
+func SetSession(w http.ResponseWriter, username string, mailid string) bool {
 	randomb := make([]byte, 32)
 	sessionid := hex.EncodeToString(randomb)
+	createdat := time.Now().UTC()
+	expires := createdat.Add(30 * time.Minute)
 
+	http.SetCookie(w, &http.Cookie{
+		Name:     sessionid,
+		Value:    sessionid,
+		Path:     "/",
+		HttpOnly: true,
+		Expires:  expires,
+	})
+
+	conn, err := db.Connect()
+	if err != nil {
+		fmt.Println("Error connecting to db in makesession.go", err)
+		return false
+	}
+
+	_, err = conn.Exec(context.TODO(), "insert into sessions (username,email,sessionid,createdat,expires) values($1,$2,$3,$4,$5)", username, mailid, sessionid, createdat, expires)
+	if err != nil {
+		fmt.Println("Error executing query for adding session to db in makesession.go", err)
+		return false
+	}
+	return true
 }
