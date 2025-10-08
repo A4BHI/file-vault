972f481d4af98168f13a63abc5c4c9e2f43ade60
A4BHI <manthukafc@gmail.com>
2025-10-08 15:14:31 +0530

    Added Login

--- go/email/verify.go
+++ go/email/verify.go
@@ -15,7 +15,7 @@ func Otp() string {
 
 func SendMail(email string, username string) error {
 
-	dm := mail.NewDialer("smtp.gmail.com", 587, "vaultx000@gmail.com", "rvcm inec dgxh eypu")
+	dm := mail.NewDialer("smtp.gmail.com", 587, "vaultx000@gmail.com", "fakevcm inec dgxh eypfake")
 	otp := Otp()
 
 	otps.StoreOtp(email, otp)
