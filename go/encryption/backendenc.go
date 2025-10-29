42489ccfa78e1459f2d30f992cf5683b0f99bcb9
A4BHI <manthukafc@gmail.com>
2025-10-29 23:09:37 +0530

    huh

--- go/encryption/backendenc.go
+++ go/encryption/backendenc.go
@@ -1,11 +1,14 @@
 package encryption
 
 import (
+	"context"
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
+	"vaultx/db"
 	"vaultx/errorcheck"
 	auth "vaultx/loginv1"
 )
@@ -16,10 +19,12 @@ func Backend_Encryption(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Requested method is not POST")
 		return
 	}
+
 	cookie, err := r.Cookie("token")
 	errorcheck.PrintError("Error getting cookie in Backend_Encryption.go", err)
 	tokenstring := cookie.Value
-	username := auth.VerifyJWT(tokenstring)
+	username := auth.VerifyJWT(tokenstring) //username from jwt
+	r.ParseMultipartForm(200 << 20)
 	file, header, err := r.FormFile("file")
 	if err != nil {
 		fmt.Println("Error in r.FormFile()", err)
@@ -49,6 +54,14 @@ func Backend_Encryption(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func AesEnc() {
+func AesEnc(file multipart.File) {
 
+}
+
+func GetMailidFromUsername(username string) string {
+	conn, err := db.Connect()
+	errorcheck.PrintError("Error connecting to db in backendenc.go file ", err)
+	var mailid string
+	conn.QueryRow(context.TODO(), "select mailid from users where username=$1", username).Scan(&mailid)
+	return mailid
 }
