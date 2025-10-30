dd82d2ee246d2c81fab07a7bc238c10af5cb39c5
A4BHI <manthukafc@gmail.com>
2025-10-30 15:55:08 +0530

    Added FileKeyEncryption func

--- go/email/verify.go
+++ go/email/verify.go
@@ -15,7 +15,7 @@ func Otp() string {
 
 func SendMail(email string, username string) error {
 
-	dm := mail.NewDialer("smtp.gmail.com", 587, "vaultx000@gmail.com", "ffvcm inec dgxh eypff")
+	dm := mail.NewDialer("smtp.gmail.com", 587, "vaultx000@gmail.com", "rvcm inec dgxh eypu")
 	otp := Otp()
 
 	otps.StoreOtp(email, otp)
--- go/encryption/backendenc.go
+++ go/encryption/backendenc.go
@@ -5,11 +5,13 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 	"vaultx/db"
 	"vaultx/errorcheck"
 	auth "vaultx/loginv1"
@@ -59,11 +61,11 @@ func Backend_Encryption(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Error copying file.", err)
 	}
 
-	AesEnc(dstpath, outputpath, mailid, filename)
+	AesEnc(dstpath, outputpath, mailid, filename, username)
 
 }
 
-func AesEnc(rawfilepath string, outputpath string, mailid string, filename string) {
+func AesEnc(rawfilepath string, outputpath string, mailid string, filename string, username string) {
 	rawfile, err := os.ReadFile(rawfilepath)
 	errorcheck.PrintError("Error reading file in Aesaenc() in backendenc.go file", err)
 
@@ -94,7 +96,10 @@ func AesEnc(rawfilepath string, outputpath string, mailid string, filename strin
 
 	os.Remove(rawfilepath)
 
-	Encrypt_Filekey(mailid, filekey)
+	file_iv := hex.EncodeToString(iv)
+
+	enc_filekey_hex, enc_filekey_iv := Encrypt_Filekey(mailid, filekey, iv)
+	StoreToFilesTable(file_iv, enc_filekey_hex, enc_filekey_iv, filename, encfilepath, username)
 
 }
 
@@ -106,21 +111,46 @@ func GetMailidFromUsername(username string) string {
 	return mailid
 }
 
-func Encrypt_Filekey(mailid string, filekey []byte) {
+func Encrypt_Filekey(mailid string, filekey []byte, fileiv []byte) (string, string) {
 	masterkey := masterkeys.LoadMasterKey(mailid)
 
 	block, err := aes.NewCipher(masterkey)
 	errorcheck.PrintError("Error creating block for encrypting fiekey", err)
 
 	gcm, err := cipher.NewGCM(block)
+	errorcheck.PrintError("Error getting gcm in Encrypt_Filekey() in backendenc.go : ", err)
 
 	filekeyiv := make([]byte, 32)
 	rand.Read(filekeyiv)
 
 	encrypted_filekey := gcm.Seal(nil, filekeyiv, filekey, nil)
 	fmt.Println(encrypted_filekey)
+
+	enc_filekey_hex := hex.EncodeToString(encrypted_filekey)
+	enc_filekey_iv := hex.EncodeToString(filekeyiv)
+
+	return enc_filekey_hex, enc_filekey_iv
+
 	// fmt.Println("MasterKey : ", masterkey)
 
 }
 
-func StoreToFilesTable
+func StoreToFilesTable(file_iv string, enc_filekey_hex string, enc_filekey_iv string, filename string, filepath string, username string) {
+
+	conn, err := db.Connect()
+	errorcheck.PrintError("Error connecting to db in backendenc.go Storetofilestable(): ", err)
+	Uploaded_At := time.Now().UTC()
+	var ownerID int
+	conn.QueryRow(context.TODO(), "Select userid from users where username=$1", username).Scan(&ownerID)
+
+	_, err = conn.Exec(
+		context.TODO(),
+		`INSERT INTO files 
+        (Owner_ID, Username, FileName, FilePath, File_IV, Enc_File_Key, Enc_File_Key_IV,Uploaded_At) 
+     VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+		ownerID, username, filename, filepath, file_iv, enc_filekey_hex, enc_filekey_iv, Uploaded_At,
+	)
+
+	errorcheck.PrintError("Error adding details to files table in StoreToFilesTable() function backendenc.go : ", err)
+
+}
