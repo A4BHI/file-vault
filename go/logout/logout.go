6ee965f122191b28a4ef6d4a0f25713e41185562
A4BHI <manthukafc@gmail.com>
2025-11-10 13:56:30 +0530

    end

--- go/logout/logout.go
+++ go/logout/logout.go
@@ -1,10 +1,13 @@
 package logout
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"time"
+	"vaultx/db"
 	auth "vaultx/loginv1"
+	"vaultx/masterkeys"
 )
 
 func Logout(w http.ResponseWriter, r *http.Request) {
@@ -31,8 +34,17 @@ func Logout(w http.ResponseWriter, r *http.Request) {
 
 	}
 	fmt.Println("Username from Logout For Debug:", username)
+	conn, err := db.Connect()
+	if err != nil {
+		fmt.Println("error in logout() cannot connect to db:", err)
+		return
+	}
 
+	var mailid string
+	conn.QueryRow(context.TODO(), "select mailid from users where username=$1", username).Scan(&mailid)
+	masterkeys.DeleteMasterkey(mailid)
 	clearCookie(w)
+
 	http.Redirect(w, r, "/index.html", http.StatusSeeOther)
 
 }
